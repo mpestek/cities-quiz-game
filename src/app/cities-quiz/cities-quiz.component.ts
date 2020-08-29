fb5007ac5dcf825352990c4cd627c8892d5da126
Mesud Pestek <mesudpestek@gmail.com>
2020-08-29 14:08:01 +0200

    Moved map from index.html to cities-quiz component and imported google maps type declarations

--- src/app/cities-quiz/cities-quiz.component.ts
+++ src/app/cities-quiz/cities-quiz.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cities-quiz.component.scss'],
 })
 export class CitiesQuizComponent implements OnInit {
+  
+  map: google.maps.Map;
 
-  constructor() { }
+  ngOnInit() {
+    this.initMap();
+  }
 
-  ngOnInit() {}
-
+  initMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 47.915774652566114, lng: 11.91214676290381 },
+      zoom: 4
+    });
+  }
 }
